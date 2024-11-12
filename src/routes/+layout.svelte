b75fb09c5dc7b987c938fe5a768208dca8457201
Ana Dodik <anadodik94@gmail.com>
2024-11-11 21:22:30 -0500

    First commit, under construction.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	
+	import { AppBar } from '@skeletonlabs/skeleton';
+	
+	export const prerender = true;
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-7">
+		<h1 class="h1">Supportive Minds Counseling</h1>
+		<p>This website is under construction.</p>
+		<p>For inquiry, contact Katherine Clemens, LICSW at kclemens@supportivemindscounseling.com or Zoi Andalcio, LMHC at zandalcio@supportivemindscounseling.com</p>
+	</div>
+</div>
